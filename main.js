111ff8414816bed110f59f0afe075ebebbc9e29c
Terak-10 <jurajstefanik77@gmail.com>
2022-05-02 16:20:45 +0200

    ...

--- main.js
+++ main.js
@@ -416,3 +416,16 @@ console.log(percentages2);
 */
 
 ////////////////////////////////  19 ////////////////////////////////
+/*
+
+listOfNeighbours = [["Canada", "Mexico"], ["Spain"],["Norway", "Sweden", "Russia"]];
+
+for(i = 0; i < listOfNeighbours.length; i++) {
+
+    for(y = 0; y < listOfNeighbours[i].length; y++) {
+        console.log(`Neigbour: ${listOfNeighbours[i][y]}`);
+    }
+}
+*/
+
+////////////////////////////////  20 ////////////////////////////////
